252b360d75fc4c86a4db947746c28d3214aacf86
Hikimory <nikita.bikon@gmail.com>
2024-10-17 13:40:17 +0300

    add homescreen

---
+++ src/app/_layout.tsx
@@ -0,0 +1,10 @@
+// import { Stack } from 'expo-router';
+// import AuthProvider from '../providers/AuthProvider';
+
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack />
+  );
+}
