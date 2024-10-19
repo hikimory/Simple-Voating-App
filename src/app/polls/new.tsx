0d9f9bde08c9aa64fb5ca34d3a12a066be076148
Hikimory <nikita.bikon@gmail.com>
2024-10-19 13:37:28 +0300

    update create poll page

--- src/app/polls/new.tsx
+++ src/app/polls/new.tsx
@@ -1,16 +1,39 @@
-import { Stack, Redirect } from 'expo-router';
+import { Redirect, Stack, router } from 'expo-router';
 import { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, Alert } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useAuth } from '../../providers/AuthProvider';
+import { supabase } from '../../lib/supabase';
 
 export default function CreatePoll() {
   const [question, setQuestion] = useState('');
   const [options, setOptions] = useState(['', '']);
+  const [error, setError] = useState('');
 
   const { user } = useAuth();
 
-  const createPoll = () => {
+  const createPoll = async () => {
+    setError('');
+    if (!question) {
+      setError('Please provide the question');
+      return;
+    }
+    const validOptions = options.filter((o) => !!o);
+    if (validOptions.length < 2) {
+      setError('Please provide at least 2 valid options');
+      return;
+    }
+
+    const { data, error } = await supabase
+      .from('polls')
+      .insert([{ question, options: validOptions }])
+      .select();
+    if (error) {
+      Alert.alert('Failed to create the poll');
+      console.log(error);
+      return;
+    }
+    router.back();
     console.warn('Create');
   };
 
@@ -48,6 +71,7 @@ export default function CreatePoll() {
             size={18}
             color="gray"
             onPress={() => {
+              // delete option based index
               const updated = [...options];
               updated.splice(index, 1);
               setOptions(updated);
@@ -57,7 +81,9 @@ export default function CreatePoll() {
         </View>
       ))}
       <Button title="Add option" onPress={() => setOptions([...options, ''])} />
+
       <Button title="Create poll" onPress={createPoll} />
+      <Text style={{ color: 'crimson' }}>{error}</Text>
     </View>
   );
 }
